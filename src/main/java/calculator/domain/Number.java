6c4263c8cc10f3d4eb130d2d75a35640c94f2bf5
sohyun <ksh96611@naver.com>
2025-10-20 17:40:08 +0900

    refactor: 숫자 파싱 로직을 Numbers에서 Number로 옮김
    숫자 각각에 대한 파싱 책임은 Number가 가지는게 맞다고 판단

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -4,6 +4,7 @@ public class Number {
     private static final int MIN_VALID_NUMBER = 1;
 
     private static final String INVALID_NUMBER_MESSAGE = "양수만 입력 가능합니다.";
+    private static final String INVALID_INPUT_MESSAGE = "잘못된 문자가 포함되어 있습니다.";
 
     private final int number;
 
@@ -12,8 +13,13 @@ public class Number {
         this.number = number;
     }
 
-    public static Number from(int number) {
-        return new Number(number);
+    public static Number from(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return new Number(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        }
     }
 
     public int valueOf() {
--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class Numbers {
     private static final String DEFAULT_DELIMITER = "[,:]";
 
-    private static final String INVALID_INPUT_MESSAGE = "잘못된 문자가 포함되어 있습니다.";
-
     private final List<Number> numbers;
 
     private Numbers(List<Number> numbers) {
@@ -15,16 +13,11 @@ public class Numbers {
     }
 
     public static Numbers extractFrom(String input) {
-        try {
-            List<Number> numbers = Arrays.stream(input.split(DEFAULT_DELIMITER))
-                    .map(Integer::parseInt)
-                    .map(Number::from)
-                    .toList();
+        List<Number> numbers = Arrays.stream(input.split(DEFAULT_DELIMITER))
+                .map(Number::from)
+                .toList();
 
-            return new Numbers(numbers);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
-        }
+        return new Numbers(numbers);
     }
 
     public int calculate() {
